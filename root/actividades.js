c33c760dd64523d335913c185d2360f815f8ad1c
Nicolas Gugliucci <gugliucci.nicolas@gmail.com>
2023-01-12 14:45:17 -0300

    actualizacion actividades

--- root/actividades.js
+++ root/actividades.js
@@ -4,14 +4,16 @@ let actividades = [
         dias: ['martes', 'jueves'],
         horas: {martes:['20:00 - 21:00','21:00 - 22:00', '22:00 - 23:00'], jueves:['20:00 - 21:00','21:00 - 22:00', '22:00 - 23:00']},
         martes: {
-            2000:{cupos:20},
-            2100:{cupos:20},
-            2200:{cupos:20}
+            2000:{cupos:1, activo: true},
+            2100:{cupos:1, activo: true},
+            2200:{cupos:1, activo: true},
+            activo: true
         },
         jueves: {
-            2000:{cupos:20},
-            2100:{cupos:20},
-            2200:{cupos:20}
+            2000:{cupos:1, activo: true},
+            2100:{cupos:1, activo: true},
+            2200:{cupos:1, activo: true},
+            activo: true
         }
     },
     ajedrez = {
@@ -19,14 +21,16 @@ let actividades = [
         dias: ['martes', 'jueves'],
         horas: {martes:['09:00 - 10:00','10:00 - 11:00', '11:00 - 12:00'], jueves:['09:00 - 10:00','10:00 - 11:00', '11:00 - 12:00']},
         martes: {
-            0900:{cupos:20},
-            1000:{cupos:20},
-            1100:{cupos:20}
+           0900:{cupos:1, activo: true},
+            1000:{cupos:1, activo: true},
+            1100:{cupos:1, activo: true},
+            activo: true
         },
         jueves: {
-            0900:{cupos:20},
-            1000:{cupos:20},
-            1100:{cupos:20}
+           0900:{cupos:20, activo: true},
+            1000:{cupos:20, activo: true},
+            1100:{cupos:20, activo: true},
+            activo: true
         }
     },
     basketbol = {
@@ -34,19 +38,22 @@ let actividades = [
         dias: ['lunes', 'miercoles', 'viernes'],
         horas: {lunes:['15:00 - 16:00','16:00 - 17:00', '17:00 - 18:00'], miercoles:['15:00 - 16:00','16:00 - 17:00', '17:00 - 18:00'], viernes:['15:00 - 16:00','16:00 - 17:00', '17:00 - 18:00']},
         lunes: {
-            1500:{cupos:20},
-            1600:{cupos:20},
-            1700:{cupos:20}
+            1500:{cupos:20, activo: true},
+            1600:{cupos:20, activo: true},
+            1700:{cupos:20, activo: true},
+            activo: true
         },
         miercoles: {
-            1500:{cupos:20},
-            1600:{cupos:20},
-            1700:{cupos:20}
+            1500:{cupos:20, activo: true},
+            1600:{cupos:20, activo: true},
+            1700:{cupos:20, activo: true},
+            activo: true
         },
         viernes: {
-            1500:{cupos:20},
-            1600:{cupos:20},
-            1700:{cupos:20}
+            1500:{cupos:20, activo: true},
+            1600:{cupos:20, activo: true},
+            1700:{cupos:20, activo: true},
+            activo: true
         }
     },
     futbol = {
@@ -54,29 +61,34 @@ let actividades = [
         dias: ['lunes', 'martes', 'miercoles', 'jueves', 'viernes'],
         horas: {lunes:['09:00 - 10:00','10:00 - 11:00', '11:00 - 12:00'], martes:['18:00 - 19:00','19:00 - 20:00', '20:00 - 21:00'], miercoles:['09:00 - 10:00','10:00 - 11:00', '11:00 - 12:00'], jueves:['18:00 - 19:00','19:00 - 20:00', '20:00 - 21:00'], viernes:['09:00 - 10:00','10:00 - 11:00', '11:00 - 12:00']},
         lunes: {
-            0900:{cupos:20},
-            1000:{cupos:20},
-            1100:{cupos:20}
+           0900:{cupos:20, activo: true},
+            1000:{cupos:20, activo: true},
+            1100:{cupos:20, activo: true},
+            activo: true
         },
         martes: {
-            1800:{cupos:20},
-            1900:{cupos:20},
-            2000:{cupos:20}
+            1800:{cupos:1, activo: true},
+            1900:{cupos:1, activo: true},
+            2000:{cupos:1, activo: true},
+            activo: true
         },
         miercoles: {
-            0900:{cupos:20},
-            1000:{cupos:20},
-            1100:{cupos:20}
+           0900:{cupos:20, activo: true},
+            1000:{cupos:20, activo: true},
+            1100:{cupos:20, activo: true},
+            activo: true
         },
         jueves: {
-            1800:{cupos:20},
-            1900:{cupos:20},
-            2000:{cupos:20}
+            1800:{cupos:20, activo: true},
+            1900:{cupos:20, activo: true},
+            2000:{cupos:20, activo: true},
+            activo: true
         },
         viernes: {
-            0900:{cupos:20},
-            1000:{cupos:20},
-            1100:{cupos:20}
+           0900:{cupos:20, activo: true},
+            1000:{cupos:20, activo: true},
+            1100:{cupos:20, activo: true},
+            activo: true
         }
     },
     gimnasia = {
@@ -84,19 +96,22 @@ let actividades = [
         dias: ['lunes', 'miercoles', 'viernes'],
         horas: {lunes:['18:00 - 19:00','19:00 - 20:00', '20:00 - 21:00'], miercoles:['18:00 - 19:00','19:00 - 20:00', '20:00 - 21:00'], viernes:['18:00 - 19:00','19:00 - 20:00', '20:00 - 21:00']},
         lunes: {
-            1800:{cupos:20},
-            1900:{cupos:20},
-            2000:{cupos:20}
+            1800:{cupos:20, activo: true},
+            1900:{cupos:20, activo: true},
+            2000:{cupos:20, activo: true},
+            activo: true
         },
         miercoles: {
-            1800:{cupos:20},
-            1900:{cupos:20},
-            2000:{cupos:20}
+            1800:{cupos:20, activo: true},
+            1900:{cupos:20, activo: true},
+            2000:{cupos:20, activo: true},
+            activo: true
         },
         viernes: {
-            1800:{cupos:20},
-            1900:{cupos:20},
-            2000:{cupos:20}
+            1800:{cupos:20, activo: true},
+            1900:{cupos:20, activo: true},
+            2000:{cupos:20, activo: true},
+            activo: true
         }
     },
     spinning = {
@@ -104,19 +119,22 @@ let actividades = [
         dias: ['lunes', 'miercoles', 'viernes'],
         horas: {lunes:['18:00 - 19:00','19:00 - 20:00', '20:00 - 21:00'], miercoles:['18:00 - 19:00','19:00 - 20:00', '20:00 - 21:00'], viernes:['18:00 - 19:00','19:00 - 20:00', '20:00 - 21:00']},
         lunes: {
-            1800:{cupos:20},
-            1900:{cupos:20},
-            2000:{cupos:20}
+            1800:{cupos:20, activo: true},
+            1900:{cupos:20, activo: true},
+            2000:{cupos:20, activo: true},
+            activo: true
         },
         miercoles: {
-            1800:{cupos:20},
-            1900:{cupos:20},
-            2000:{cupos:20}
+            1800:{cupos:20, activo: true},
+            1900:{cupos:20, activo: true},
+            2000:{cupos:20, activo: true},
+            activo: true
         },
         viernes: {
-            1800:{cupos:20},
-            1900:{cupos:20},
-            2000:{cupos:20}
+            1800:{cupos:20, activo: true},
+            1900:{cupos:20, activo: true},
+            2000:{cupos:20, activo: true},
+            activo: true
         }
     },
     voley = {
@@ -124,26 +142,31 @@ let actividades = [
         dias: ['lunes', 'martes', 'miercoles', 'jueves', 'viernes'],
         horas: {lunes:['21:00 - 22:00','22:00 - 23:00'], martes:['15:00 - 16:00', '16:00 - 17:00', '17:00 - 18:00'], miercoles:['21:00 - 22:00','22:00 - 23:00'], jueves:['15:00 - 16:00', '16:00 - 17:00', '17:00 - 18:00'], viernes:['21:00 - 22:00','22:00 - 23:00']},
         lunes: {
-            2100:{cupos:20},
-            2200:{cupos:20}
+            2100:{cupos:20, activo: true},
+            2200:{cupos:20, activo: true},
+            activo: true
         },
         martes: {
-            1500:{cupos:20},
-            1600:{cupos:20},
-            1700:{cupos:20}
+            1500:{cupos:1, activo: true},
+            1600:{cupos:1, activo: true},
+            1700:{cupos:1, activo: true},
+            activo: true
         },
         miercoles: {
-            2100:{cupos:20},
-            2200:{cupos:20}
+            2100:{cupos:20, activo: true},
+            2200:{cupos:20, activo: true},
+            activo: true
         },
         jueves: {
-            1500:{cupos:20},
-            1600:{cupos:20},
-            1700:{cupos:20}
+            1500:{cupos:20, activo: true},
+            1600:{cupos:20, activo: true},
+            1700:{cupos:20, activo: true},
+            activo: true
         },
         viernes: {
-            2100:{cupos:20},
-            2200:{cupos:20}
+            2100:{cupos:20, activo: true},
+            2200:{cupos:20, activo: true},
+            activo: true
         }
     },
     
@@ -179,5 +202,9 @@ function disponibilidad(actividad_escogida, dia, hora){
 
 function restar_cupo(actividad_escogida, dia, hora){
     actividades[actividades.indexOf(actividades.find((actividad) => actividad.nombre.toLocaleUpperCase() == actividad_escogida.toLocaleUpperCase()))][dia][hora].cupos--;
+}
+
+function sumar_cupo(actividad_escogida, dia, hora){
+    actividades[actividades.indexOf(actividades.find((actividad) => actividad.nombre.toLocaleUpperCase() == actividad_escogida.toLocaleUpperCase()))][dia][hora].cupos++;
 }
 
